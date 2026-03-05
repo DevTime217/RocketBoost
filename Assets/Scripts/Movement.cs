053ff170e5708bbd31ebf1007311344d5def0f62
Adam Malik <headshotgamingmaster@gmail.com>
2026-03-05 15:03:45 -0500

    Created Movement Script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+    }
+
+    private void OnDisable()
+    {
+        thrust.Disable();
+    }
+
+    private void Update()
+    {
+        if(thrust.IsPressed())
+        {
+            Debug.Log("Thrusting");
+        }
+    }
+}
